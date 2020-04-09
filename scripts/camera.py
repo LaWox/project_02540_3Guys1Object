0a994515b584a7e0fca262f3bff328bddb3bc8f4
LaWox <woxler.platon@gmail.com>
2020-04-09 17:57:02 +0200

    can now save intristic parameters

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,8 +1,6 @@
 import numpy as np 
 import cv2 
-import calibration 
 import glob
-
 
 BOARD_DIMS = (22, 13)
 # BOARD_DIMS = (8, 6)
@@ -17,25 +15,40 @@ def getObjPoints(squareLength = 30):
     return np.asarray(objPoints)
 
 class Camera:
-    def __init__(self, calibrationImgPath, objImgPath, K = []):
-        self.K = []                                  # Intristic matrix of camera
-        self.objImages = objImgPath                  # path to cameras picture of object 
+    def __init__(self, calibrationImgPath, objImgPath, cameraNr = 0, K = [], calibrated = False):
+        self.K = []
+        self.objImgPath = objImgPath                 # path to cameras picture of object 
         self.calibrationImgPath = calibrationImgPath # path to folder where calibration images are
-        pass
+        self.calibrationPoints = []
+        self.objPoints = []
+        self.cameraNr = cameraNr
 
-    def calibrate(self):
+        if calibrated:
+            self.K = self.__getKFromFile() # get calibrated K from file
+            self.calibrationPoints = self.__getCalibrationPointsFromFile()
+            self.objPoints = self.__getObjPointsFromFile()
+
+        else: # save new data if not already calculated
+            self.__calibrate() 
+            self.__saveK() 
+            self.__saveCalibrationPoints()
+            self.__saveObjPoints()
+        return 
+
+    def __calibrate(self):
         # termination criteria
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
         # calibrate camera with checkboard images 
-        images = [cv2.imread(file) for file in glob.glob(self.calibrationImgPath + "*.png")]
+        images = self.__getCalibrationImages()
         objPoints = []
         imgPoints = []
+        gray = []
 
         objP = getObjPoints() # make checkersPoints 
-        
+
         counter = 1 
-        for img in images:            
+        for img in images:
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             ret, corners = cv2.findChessboardCorners(gray, BOARD_DIMS, None)
             
@@ -51,33 +64,56 @@ class Camera:
         
         objPoints = np.asarray(objPoints).astype(np.float32)
         imgPoints = np.asarray(imgPoints)
-        
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints, gray.shape[::-1],None,None)
 
-        '''
-        # checking error 
-        tot_error =  0
-        for i in range(len(objPoints)):
-            imgpoints2, _ = cv2.projectPoints(objPoints[i], rvecs[i], tvecs[i], mtx, dist)
-            error = cv2.norm(imgPoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-            tot_error += error
-        print ("mean error: ", tot_error/len(objPoints))
-        '''
-        
+        self.objPoints = objPoints
+        self.calibrationPoints = imgPoints
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints, gray.shape[::-1],None,None)
+    
         self.K = mtx
         return 
 
+    def __saveK(self):
+        np.save("data/cameraData/camera" + str(self.cameraNr) + '/kMatrix', self.K)
+        return 
+
+    def __saveCalibrationPoints(self):
+        np.save("data/cameraData/camera" + str(self.cameraNr) + '/calibrationPoints', self.calibrationPoints)
+    
+    def __saveObjPoints(self):
+        np.save("data/cameraData/camera" + str(self.cameraNr) + '/objPoints', self.objPoints)
+    
+    def __getKFromFile(self):
+        K = np.load("data/cameraData/camera" + str(self.cameraNr) + '/kMatrix' + '.npy')
+        return K  
+
+    def __getCalibrationPointsFromFile(self):
+        points = np.load("data/cameraData/camera" + str(self.cameraNr) + '/calibrationPoints' + '.npy')
+        return points
+
+    def __getObjPointsFromFile(self):
+        points = np.load("data/cameraData/camera" + str(self.cameraNr) + '/objPoints' + '.npy')
+        return points
+
+    def __getCalibrationImages(self):
+        images = [cv2.imread(file) for file in glob.glob(self.calibrationImgPath + "*.png")]
+        return images 
+
+    def getCalibrationPoints(self):
+        return self.calibrationPoints
+
+    def getObjPoints(self):
+        return self.objPoints
+
     def getImages(self):
         # get images associated with this camera from path parameter
-        pass
+        images = [cv2.imread(file) for file in glob.glob(self.objImgPath + "*.png")]
+        return images
 
     def getK(self):
         # return intristic matrix
         return self.K
 
 if __name__ == "__main__":
-    calibrationPath = "../data/calibrationImgs/camera0/"
+    calibrationPath = "data/calibrationImgs/camera0/"
     objPath = "hejehje"
-    camera = Camera(calibrationPath, objPath)
-    camera.calibrate()
-    print(camera.getK())
+    camera = Camera(calibrationPath, objPath, calibrated=True)
--- scripts/cameraRig.py
+++ scripts/cameraRig.py
@@ -1,10 +1,16 @@
 import numpy as np 
 import camera
+import cv2 
+
+IMG_SHAPE = (1850, 1137)
 
 class Rig:
-    def __init__(self, cameras = []):
+    def __init__(self, cameras):
         self.cameras = cameras
         self.homoGraphies = {}
+        self.objP = cameras[0].getObjPoints() # uses square len of 30
+
+        # init homographies between the cameras
         self.initHompgraphies()
         return 
     
@@ -13,7 +19,19 @@ class Rig:
         noCameras = len(self.cameras) # number of cameras in rig setup 
         for i in range(noCameras):
             for j in range(i+1, noCameras):
-                # compute homogrpaphy here
+                camera1 = self.cameras[i]
+                camera2 = self.cameras[j]
+
+                retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(
+                    camera1.getObjPoints(),
+                    camera1.getCalibrationPoints(),
+                    camera2.getCalibrationPoints(), 
+                    IMG_SHAPE,
+                    camera1.getK(),
+                    camera2.getK(),
+                    None,
+                    None
+                    )
                 self.homoGraphies[str(i) + str(j)] = 1 # get homogrpahy here
     
     
@@ -21,12 +39,12 @@ class Rig:
     def getHomography(self, cam1, cam2):
         return self.homoGraphies[str(cam1) + str(cam2)]
 
-
 if __name__ == "__main__":
-    cPath1 = "../data/calibrationImgs/camera0/"
-    cPath2 = "../data/calibrationImgs/camera1/"
+    cPath1 = "data/calibrationImgs/camera0/"
+    cPath2 = "data/calibrationImgs/camera1/"
 
-    camera1 = camera.Camera(cPath1, "hejehj")
-    camera2 = camera.Camera(cPath2, "hejehj")
+    camera1 = camera.Camera(cPath1, "hejehj", cameraNr = 0, calibrated = True)
+    camera2 = camera.Camera(cPath2, "hejehj", cameraNr = 1, calibrated = True)
     
-    newRig = Rig([camera1, camera2])    
+    newRig = Rig([camera1, camera2])  
+    newRig.initHompgraphies()  
