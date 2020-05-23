93846e0cb0a811d09c7512f2634336b4f8e3f533
wileik15 <william.eikrem@hotmail.no>
2020-05-23 10:49:32 +0200

    dd

--- scripts/pointcloudStitching.py
+++ scripts/pointcloudStitching.py
@@ -20,27 +20,33 @@ def drawRegistrations(source, target, transformation = None, recolor = False):
 
 
 def globalRegistration(source, target, voxel_size):
+    #Checks for these correspondences in ransac loops
+    corr_check = [o3d.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)]
 
+    #Down sampling the point cloud for faster computation
     source_sample = source.voxel_down_sample(voxel_size)
     target_sample = target.voxel_down_sample(voxel_size)
 
+    #Estimating the normals for each point in cloud, to use to find fpfh features
     source_sample.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius = 2 * voxel_size, max_nn = 100))
     target_sample.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius = 2 * voxel_size, max_nn = 100))
 
+    #Finding the fpfh feature histograms
     source_fpfh = o3d.registration.compute_fpfh_feature(source_sample, o3d.geometry.KDTreeSearchParamHybrid(radius = 2 * voxel_size, max_nn = 100))
     target_fpfh = o3d.registration.compute_fpfh_feature(target_sample, o3d.geometry.KDTreeSearchParamHybrid(radius=2 * voxel_size, max_nn=100))
 
-    point_to_point = o3d.io.registration.TransformationEstimationPointToPoint(False)
+    #Defining the error measure for the registration by ransac
+    point_to_point = o3d.registration.TransformationEstimationPointToPoint(False)
     distance_threshold = 1.5*voxel_size
-    corr_check = [o3d.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)]
 
+    #The final result of transformation between the two pointclouds
     ransac_result = o3d.registration.registration_ransac_based_on_feature_matching(source_sample, target_sample, source_fpfh, target_fpfh,
                                                                                    distance_threshold, point_to_point, checkers = corr_check)
     return ransac_result
 
 
-cld1 = o3d.io.read_point_cloud("data/3D_point_cloud_test/r1.pcd")
-cld2 = o3d.io.read_point_cloud("data/3D_point_cloud_test/r2.pcd")
+cld1 = o3d.io.read_point_cloud("/Users/william/PycharmProjects/project_02540_3Guys1Object/data/3D_point_cloud_test/r1.pcd")
+cld2 = o3d.io.read_point_cloud("/Users/william/PycharmProjects/project_02540_3Guys1Object/data/3D_point_cloud_test/r1.pcd")
 
 ransac_result = globalRegistration(cld1, cld2, 0.05)
 trans = ransac_result.transformation
@@ -48,13 +54,15 @@ trans = ransac_result.transformation
 drawRegistrations(cld1, cld2, trans, True)
 
 def ICP(source, target, threshold, rad, maxnn):
-
+    #Initial transformation
     trans_init = np.asarray([[1, 0, 0, o], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
     #Evaluate initial alignment?
 
+    #Defining the error measure
     point_to_plane = o3d.registration.TransformationEstimationPointToPlane()
 
+    #Estimating the normals for each point, to define planes in the point to plane algorithm
     source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
         radius=rad, max_nn=maxnn), fast_normal_computation=True)
     target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
@@ -72,4 +80,5 @@ icp_result = ICP(source_aligned, target_aligned, 0.1, 0.5, 300)
 
 icp_trans = icp_result.transformation
 
-drawRegistrations()
+#Source and target stitched together
+Stitched = target + source.transform(icp_trans)
